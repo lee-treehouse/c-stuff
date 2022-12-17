d9ffb4def4b22f746440abc3a1a3971e5bfe954b
lee-treehouse <leentaylor@gmail.com>
2022-12-17 16:27:58 +1100

    plurality and credit progress commit - passing check50 tests but need tidying up

--- cs50Exercises/week1/credit/credit.c
+++ cs50Exercises/week1/credit/credit.c
@@ -3,12 +3,75 @@
 
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+// not ascii number but actual integer value 
+int getIntFromChar(char value)
+{
+    return (int)(value) - 48;
+}
+
+// string getStringFromInt(int value)
+// {
+//     char result[20];
+//     sprintf(result, "%d", value);
+//     return result;
+// }
+
+int getSumOfEachDigitsInInteger(int value)
+{
+        //string valueAsString = getStringFromInt(value);
+
+    char valueAsString[20];
+    sprintf(valueAsString, "%d", value);
+
+        //printf ("string value from %d is %s \n", value, valueAsString);
+        int sum = 0;
+        for (int i = 0; i < strlen(valueAsString); i++)
+        {
+            sum += getIntFromChar(valueAsString[i]);
+        }
+        return sum;
+}
 
 bool isValidNumber(string creditCardNumber)
 {
-    // TODO 
-    return true;    
+    int cardNumberLength = strlen(creditCardNumber);
+
+    // Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together.
+    int sumStep1 = 0;
+    for (int i=cardNumberLength-2; i >= 0; i-= 2)
+    {
+        int value = getIntFromChar(creditCardNumber[i]);
+
+        int doubleValue = value * 2;        
+        
+        sumStep1 += getSumOfEachDigitsInInteger(doubleValue);
+        //printf("%d \n", value);
+    }
+
+
+    // sum the digits that weren’t multiplied by 2.
+    int sumStep2 = 0;
+    for (int i=cardNumberLength-1; i >= 0; i-= 2)
+    {
+        int value = getIntFromChar(creditCardNumber[i]);
+        sumStep2 += value;
+        //printf("%d \n", value);
+    }
+
+    // sum the results of step 1 and the results of step 2
+    int finalSum = sumStep1 + sumStep2;
+
+    if (finalSum % 10 == 0)
+    {
+        //printf("%s is Valid \n", creditCardNumber);
+        return true;
+    }
+
+    //printf("%s is not Valid \n", creditCardNumber);
+    return false;    
 }
 
 string getCardType(string creditCardNumber)
@@ -28,6 +91,7 @@ string getCardType(string creditCardNumber)
     // AMEX: 15 digits; first two digits are 34 or 37
     if (numberLength == 15 && firstNumber == threeInASCII && (secondNumber == fourInASCII || secondNumber == sevenInASCII))
     {
+        //printf("%s is Amex \n", creditCardNumber);
         return "AMEX";
     }
 
@@ -37,6 +101,7 @@ string getCardType(string creditCardNumber)
         int secondNumberAsInt = (int)secondNumber;
         if (secondNumberAsInt >= oneInASCII && secondNumberAsInt <= fiveInASCII)
         {
+        //printf("%s is Mastercard \n", creditCardNumber);
             return "MASTERCARD";
         }
     }
@@ -44,9 +109,11 @@ string getCardType(string creditCardNumber)
     // Visa: 13 or 16 digits; first digit is 4 (52 in ASCII)
     if ((numberLength == 13 || numberLength == 16) && firstNumber == fourInASCII)
     {
+       // printf("%s is VISA \n", creditCardNumber);
         return "VISA";
     }
 
+    //printf("%s is invalid cardtype \n", creditCardNumber);
     return "";
 }
 
@@ -58,16 +125,16 @@ int main(void)
  
  if (strcmp(cardType, "") == 0)
  {
-    printf("INVALID");
+    printf("INVALID\n");
  } else 
  {
     bool isValid = isValidNumber(creditCardNumber);
     if (!isValid)
     {
-        printf("INVALID");
+        printf("INVALID\n");
     } else 
     {
-        printf("%s", cardType);
+        printf("%s\n", cardType);
     }
  }
  
---
+++ cs50Exercises/week3/plurality/plurality.c
@@ -0,0 +1,134 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Max number of candidates
+#define MAX 9
+
+// Candidates have name and vote count
+typedef struct
+{
+    string name;
+    int votes;
+}
+candidate;
+
+// Array of candidates
+candidate candidates[MAX];
+
+// Number of candidates
+int candidate_count;
+
+// Function prototypes
+bool vote(string name);
+void print_winner(void);
+
+int main(int argc, string argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    // Populate array of candidates
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        // printf("candidate %i name being set to %s \n", i, argv[i + 1]);
+        candidates[i].votes = 0;
+        // printf("candidate %i votes being set to 0 \n", i);
+    }
+
+    int voter_count = get_int("Number of voters: ");
+
+    // Loop over all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        // Check for invalid vote
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+
+    // Display winner of election
+    print_winner();
+}
+
+// Update vote totals given a new vote
+bool vote(string name)
+{
+
+//candidates
+    bool candidateFound = false;
+    // printf("candidate count is %d \n", candidate_count);
+    for (int i=0; i < candidate_count; i++)
+    {
+        string nameToCheck = candidates[i].name;
+        if  (strcmp(name, nameToCheck) == 0)
+        {
+            // printf("match found - %s matches %s, incremeneting votes \n", name, nameToCheck);
+            candidates[i].votes++;
+            candidateFound = true;
+        }
+        // } else 
+        // {
+        //     printf("match not found - %s doesn't matches %s \n", name, nameToCheck);
+        // }
+    }
+    // TODO
+    return candidateFound;
+}
+
+// Print the winner (or winners) of the election
+void print_winner(void)
+{
+    //determine winning number of votes
+    int highestNumberVotes = 0;
+    for (int i=0; i < candidate_count; i++)
+    {
+        int currentVoteCount = candidates[i].votes;
+        if (currentVoteCount > highestNumberVotes)
+        {
+            highestNumberVotes = currentVoteCount;
+        }
+    }
+  
+    // count the winners to be able to size array (maybe there's a better way, but start here )
+    int winnerCount = 0;
+    for (int i=0; i < candidate_count; i++)
+    {
+        int currentVoteCount = candidates[i].votes;
+        if (currentVoteCount == highestNumberVotes)
+        {
+            winnerCount ++;
+        }
+    }
+
+    // print winner(s)
+    int winnersOutputted = 0;
+    for (int i=0; i < candidate_count; i++)
+    {
+        int currentVoteCount = candidates[i].votes;
+        if (currentVoteCount == highestNumberVotes)
+        {
+            printf("%s", candidates[i].name);
+            winnersOutputted++;
+            if (winnersOutputted == 1 || winnersOutputted < winnerCount)
+            {
+                printf("\n");
+            }
+        }
+    }
+    return;
+}
